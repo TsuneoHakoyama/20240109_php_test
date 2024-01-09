6922bb1d5260a5cd8c62fcbdf594a5b4d04c5db9
Tsuneo Hakoyama <tunehako@gmail.com>
2024-01-09 22:52:23 +0900

    リモートリポジトリの変更

---
+++ src/php01/index02.php
@@ -0,0 +1,20 @@
+<?php
+echo 20;
+echo "<br />";
+echo "こんにちは";
+echo "<br />";
+
+$name1 = "Sato" . "Taro";
+$name2 ="Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name1 . "<br/>";
+echo $name2 . "Jiro" . "<br/>";
+echo $last_name . $first_name;
+echo "<br />";
+
+$name = "Tanaka";
+
+echo $name . "Jiro" . "<br/>";
+echo $last_name . $first_name;
